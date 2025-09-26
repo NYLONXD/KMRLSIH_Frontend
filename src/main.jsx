e84f62507720b2c68d273f8fefe4c5b90c77b7a2
shwewinaung <nickytwist70@gmail.com>
2025-09-26 10:26:08 +0530

    added dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App.jsx";
 import "./i18n";
+import "atropos/css";
 
 createRoot(document.getElementById("root")).render(
   <LanguageProvider>
